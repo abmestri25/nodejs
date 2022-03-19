14ff8e6dbb370cd381e68539d2a541ca5e3eefd6
Abhishek Mestri <abmestri25@gmail.com>
2022-03-19 18:31:25 +0530

    created server and added pages and redirection

---
+++ server.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const fs = require('fs');
+
+
+const server = http.createServer((req,res) => {
+
+    // setting header content
+    res.setHeader('Content-Type','text/html');
+  
+    // multiple routes
+    let path = './views/';
+
+    switch(req.url){
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location','/about');
+            break;
+        default:
+            path += '404.html'
+            res.statusCode = 404;
+            break;
+    }
+    // sending html file
+    fs.readFile(path,(err,data) => {
+        if(err){
+            console.log(err);
+            res.end()
+        }else{
+            res.end(data);  
+        }
+    });
+})
+
+server.listen(3000,'localhost', () => {
+    console.log('Listening for requests on PORT 3000');
+})
