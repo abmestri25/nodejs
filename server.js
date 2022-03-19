0790de282b076addff15f0791ea553955b1776bd
Abhishek Mestri <abmestri25@gmail.com>
2022-03-19 18:58:41 +0530

    install lodash and nodemon

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const http = require('http');
 const fs = require('fs');
+const _ = require('lodash');
 
 
 const server = http.createServer((req,res) => {
+
+    // to get random number
+    const num = _.random(0,100);
+    console.log(num);
+
+    // to load function only once
+    const greet = _.once(() => {
+        console.log('Hello There !');
+    })
+
+    greet();
+    // This will not get called as we have used once() from lodash
+    greet();
 
     // setting header content
     res.setHeader('Content-Type','text/html');
@@ -19,7 +33,7 @@ const server = http.createServer((req,res) => {
             path += 'about.html';
             res.statusCode = 200;
             break;
-        case '/about-me':
+        case '/about-blog':
             res.statusCode = 301;
             res.setHeader('Location','/about');
             break;
